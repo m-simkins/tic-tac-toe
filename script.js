87c9a50367c8b8a2bc186b54dd428ef30e3a57a0
mike simkins <185857345+m-simkins@users.noreply.github.com>
2025-02-14 18:35:36 -0500

    actually move win checker into gameboard

--- script.js
+++ script.js
@@ -13,9 +13,37 @@ function Gameboard() {
     }
   }
   
-  
+  const checkRow = (row, player) => board[row].every((v) => v === player.symbol);
 
-  return { getBoard, markSquare }
+  const checkCol = (col, player) => {
+    const arr = [
+      board[0][col],
+      board[1][col],
+      board[2][col]
+    ];
+    return arr.every((v) => v === player.symbol);
+  }
+
+  const checkLtr = (player) => {
+    const ltr = [board[0][0], board[1][1], board[2][2]];
+    return ltr.every((v) => v === player.symbol);
+  }
+
+  const checkRtl = (player) => {
+    const rtl = [board[2][0], board[1][1], board[0][2]];
+    return rtl.every((v) => v === player.symbol);
+  }
+
+  const checkWin = (row, col, player) => {
+    if (checkRow(row, player) || checkCol(col, player) || checkRtl(player) || checkLtr(player))
+        {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return { getBoard, markSquare, checkWin }
 }
 
 function Player(name, symbol) {
@@ -38,35 +66,3 @@ const player2 = Player("player2", "O");
 game.markSquare(player1, 0, 2);
 
 console.log(board);
-
-function checkRow(row, player) {
- return board[row].every((v) => v === player.symbol);
-}
-
-function checkCol(col, player) {
-  const arr = [
-    board[0][col],
-    board[1][col],
-    board[2][col]
-  ];
-  return arr.every((v) => v === player.symbol);
-}
-
-function checkLtr(player) {
-  const ltr = [board[0][0], board[1][1], board[2][2]];
-  return ltr.every((v) => v === player.symbol);
-}
-
-function checkRtl(player) {
-  const rtl = [board[2][0], board[1][1], board[0][2]];
-  return rtl.every((v) => v === player.symbol);
-}
-
-function checkWin(row, col, player) {
-  if (checkRow(row, player) || checkCol(col, player) || checkRtl(player) || checkLtr(player))
-     {
-    return true;
-  } else {
-    return false;
-  }
-}
